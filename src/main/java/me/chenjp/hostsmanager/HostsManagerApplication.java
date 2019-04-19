e18e9d249282ff271d62895b7a65491b90f234a3
chenjunping <chenjunping@ad61v1.com>
2019-04-19 18:31:11 +0800

    chenjp: 初始化项目

---
+++ src/main/java/me/chenjp/hostsmanager/HostsManagerApplication.java
@@ -0,0 +1,13 @@
+package me.chenjp.hostsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostsManagerApplication.class, args);
+    }
+
+}
